101a5b093956544d0e4f0e5c7bde6b98115cda34
zhangyachong <779868735@qq.com>
2020-07-31 15:22:11 +0800

    20200731-1522

---
+++ sort/969. Pancake Sorting.py
@@ -0,0 +1,31 @@
+from typing import List
+import sys
+class Solution:
+    def pancakeSort(self, A: List[int]) -> List[int]:
+        result = []
+        self.pancake(A, len(A) - 1,result)
+        return result
+    def pancake(self, arr, n, result):
+        if n <= 0:
+            return
+        maxIndex = 0
+        maxVal = -sys.maxsize - 1
+        for i in range(n):
+            if arr[i] > maxVal:
+                maxVal = arr[i]
+                maxIndex = i
+        self.reverse(arr, 0, maxIndex)
+        result.append(maxIndex + 1)
+        self.reverse(arr, 0, n - 1)
+        result.append(n)
+        self.pancake(arr, n - 1, result)
+    
+    def reverse(self, arr, start, end):
+        while start < end:
+            arr[start], arr[end] = arr[end], arr[start]
+            start += 1
+            end -= 1
+if __name__ == "__main__":
+    s = Solution()
+    res = s.pancakeSort([3,2,4,1])
+    print(res)
