ed88b8b1cb35ad38ef0763f4faa7cadd686ac067
zhangyachong <zhangyachong@meituan.com>
2022-01-31 11:44:23 +0800

    1

--- java/19. Remove Nth Node From End of List.java
+++ java/19. Remove Nth Node From End of List.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -28,27 +31,41 @@ class ListNode {
 class Solution {
 
     public static void main(String[] args) {
-        List<Integer> list =
-        [1,2,3,4,5]
+        List<Integer> list = new ArrayList<>();
+//        list.add(2);
+//        list.add(3);
+//        list.add(4);
+//        list.add(5);
+        ListNode current = new ListNode();
+        current.val = 1;
+        ListNode head = current;
+        for (Integer i : list) {
+            ListNode tmp  = new ListNode();
+            tmp.val = i;
+            current.next = tmp;
+            current = tmp;
+        }
+        ListNode listNode = removeNthFromEnd(head, 1);
+        System.out.println(1);
     }
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode pre = null;
-        ListNode end = head.next;
-        int currNum = 0;
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode pre = new ListNode();
+        pre.next = head;
+        ListNode end = head;
+        int currNum = n;
         while(null != end) {
-            if (currNum < n) {
-                currNum++;
-                end = end.next;
-            } else if (currNum == n) {
-                pre = end;
+            if (currNum > 0) {
+                currNum--;
             } else {
-                pre =  pre.next;
+                pre = pre.next;
             }
             end = end.next;
         }
-        if (currNum > n) {
+        if (null != pre.next && pre.next != head) {
             pre.next = pre.next.next;
+            return head;
+        } else {
+            return head.next;
         }
-        return head;
     }
 }
